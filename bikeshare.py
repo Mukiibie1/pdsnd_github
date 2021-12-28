b4a78fcc51a9591e2fbbe6164fcfece013b68d40
Mukiibi Eli <kpmukiibi@gmail.com>
2021-12-28 21:30:44 +0300

    refactor : add most frequent combinationof start station and end station trip

--- bikeshare.py
+++ bikeshare.py
@@ -126,7 +126,8 @@ def station_stats(df):
     print("{} is the most commonly used end station.".format(common_end_station))
 
     # display most frequent combination of start station and end station trip
-
+    freq_start_end_station = (df['Start Station'] + ' - ' + df['End Station']).value_counts().idxmax()
+    print('Most frequent combination of start station and end station trip {}'.format(freq_start_end_station))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
