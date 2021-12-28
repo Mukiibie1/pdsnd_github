436c23330af070d467ae0d242fc978a65bb20e6e
Mukiibi Eli <kpmukiibi@gmail.com>
2021-12-28 21:23:43 +0300

    refactor: add most commonlt used end station

--- bikeshare.py
+++ bikeshare.py
@@ -118,10 +118,12 @@ def station_stats(df):
     start_time = time.time()
 
     # display most commonly used start station
-
+    common_start_station = df['Start Station'].value_counts().idxmax()
+    print("{} is the most commonly used start station.".format(common_start_station))
 
     # display most commonly used end station
-
+    common_end_station = df['End Station'].value_counts().idxmax()
+    print("{} is the most commonly used end station.".format(common_end_station))
 
     # display most frequent combination of start station and end station trip
 
